6f499a812b0364e9a3be21c3c3d2aada78d3a1b0
Joshua MacDonald <jmacd@lightstep.com>
2024-01-21 22:55:16 -0800

    renumber epwm/ecap events

--- examples/led_outin/led_outin.c
+++ examples/led_outin/led_outin.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 
 #include "lib/args/args.h"
-#include "lib/cap/cap.h"
 #include "lib/coroutine/coroutine.h"
 #include "lib/debug/debug.h"
 #include "lib/gpio/gpio.h"
@@ -11,7 +10,6 @@
 #include "lib/intc/service.h"
 #include "lib/log/daemon/daemon.h"
 #include "lib/pinmap/pinmap.h"
-#include "lib/pwm/pwm.h"
 #include "lib/resource/table.h"
 #include "lib/rpmsg/rpmsg.h"
 #include "lib/time/clock.h"
@@ -65,8 +63,6 @@ int main(void) {
   GPIO_Init();
   SyslogInit();
   ProcessInit();
-  CAP_Init();
-  PWM_Init();
 
   args.ptr = "0";
 
--- lib/pwm/am335x/pwm-am335x.c
+++ lib/pwm/am335x/pwm-am335x.c
@@ -13,7 +13,7 @@ void PWM_Init(void) {
                       (0 << 10) | // CLKDIV: Time-base clock prescale bits
                       (1 << 7) |  // HSPCLKDIV: (Default) high-speed clock prescale bits
                       (0 << 6) |  // SWFSYNC: No force sync pulse
-                      (1 << 4) |  // SYNCOSEL: Sync output when TBCNT == 0
+                      (0 << 4) |  // SYNCOSEL: Sync output from EPWMxSYNC
                       (0 << 3) |  // PRDLD: Load active period from shadow
                       (0 << 2) |  // PHSEN: Do not load from time-base phase register
                       (0 << 0);   // CTRMODE: Up-count mode
@@ -24,7 +24,7 @@ void PWM_Init(void) {
   // TBCNT: Not used
 
   //////////////////////////////////////////////////////////////////////
-  // Counter-compare
+  // Counter compare
 
   // CMPA: Not used
   // CMPAHR: Not used
@@ -33,16 +33,26 @@ void PWM_Init(void) {
 
   // CMPCTL: All defaults
   // CMPAHR: Not used
-  PWMSS0.EPWM_CMPA = 500; // CMPB: Compare value for SYNCOSEL.
-  // CMPB: Not used.
+  PWMSS0.EPWM_CMPA = 500; // CMPB: Compare value for clearing EPWMxA
+  PWMSS0.EPWM_CMPB = 750; // CMPB: Compare value for sample interrupt
 
   //////////////////////////////////////////////////////////////////////
-  // Action-qualifier
+  // Action qualifier
 
   PWMSS0.EPWM_AQCTLA = (2 << 0) | // ZRO: force EPWMxA high when TBCNT == 0
                        (1 << 4);  // CAU: force EPWMxA low when TBCNT == CMPA
   // AQCTLB: Not used
   // AQSFRC: Not used
   // AQCSFRC: Not used
-  // DBCTL: Not used
+
+  //////////////////////////////////////////////////////////////////////
+  // Chopper: Not used
+  // Deadband: Not used
+
+  //////////////////////////////////////////////////////////////////////
+  // Event trigger
+
+  PWMSS0.EPWM_ETSEL = (1 << 3) | // INTEN: Enable interrupt
+                      (6 << 0);  // INTSEL: CMPB incrementing
+  PWMSS0.EPWM_ETPS = (1 << 0);   // INTPRD: Every event
 }
